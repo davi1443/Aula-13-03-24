f0e883903f6ad74ac81c4de84d230b2b57e2ea3d
Davi Costa <103383379+davi1443@users.noreply.github.com>
2024-03-13 20:24:13 -0300

    add javascript

---
+++ tigrinho.js
@@ -0,0 +1,19 @@
+function deposite10(){
+    alert("O depósito de 10 reais foi feito")
+}
+
+function deposite100(){
+    alert("O depósito de 100 reais foi feito")
+}
+
+function deposite500(){
+    alert("O depósito de 500 reais foi feito")
+}
+
+function deposite1000(){
+    alert("O depósito de 1000 reais foi feito")
+}
+
+function pix(){
+    alert("A sua quantia foi sacada")
+}
